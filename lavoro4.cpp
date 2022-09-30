0ecad16c1c1c677abeda8f476f06ff9fc73ac7f3
l-savi <113691727+l-savi@users.noreply.github.com>
2022-09-30 13:56:30 +0200

    Add files via upload

---
+++ lavoro4.cpp
@@ -0,0 +1,31 @@
+/*
+//File: area
+Autore:Leonardo Savino
+Classe:3H
+data: 30/09/2022
+
+calcola l'area del cerchio 
+
+Input: raggio
+Elaborazione: calcola la circoferenza e l'area 
+Output: area e circoferenza 
+*/
+
+
+#include <stdio.h> //printf,scanf
+#defline P_GRECO 3,14
+
+int main()
+{
+	//dichiariamo le due variabili input
+	float p_greco=3,14;
+	float raggio,circonferenza;
+	//avvalorare input chiedendo all'utente
+	printf("inseriscila misura del raggio")
+	scanf("x%" , &raggio);
+	
+	//elaborazione
+	area=(raggio*raggio)*P_GRECO;
+	circonferenza=(2*p_greco)
+	//output
+	printf
