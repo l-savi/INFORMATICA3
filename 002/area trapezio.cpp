16e2731c655a0fc28936b701e851f6735dd69cb2
l-savi <113691727+l-savi@users.noreply.github.com>
2022-10-19 13:59:55 +0200

    Update area trapezio.cpp

--- 002/area trapezio.cpp
+++ 002/area trapezio.cpp
@@ -11,7 +11,8 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main() 
+{
   int baseM, basem, altezza;  //input
   float latoObliquo, diff;  //lavoro
   float area, perimetro;  //output
