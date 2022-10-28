7fe73a0f67dc5265c30bb057f89f6a5a4225ed12
l-savi <113691727+l-savi@users.noreply.github.com>
2022-10-28 18:46:14 +0200

    Add files via upload

---
+++ 001/d.minoreemaggiore.c
@@ -0,0 +1,51 @@
+/*/** ****************************************************************************************
+* \mainpage d.minoreemaggiore.c
+*
+* @brief Scrivere un programma che, richiesti in ingresso due valori interi distinti, ne stampi a schermo il minore.
+* 
+* 
+* @author Leonardo Savino
+* @date 28/10/2022
+*/
+
+
+#include <stdio.h>
+
+int main() {
+	
+  int a,b,c;	//input
+  int max, min;	//output
+  	
+  printf("inserisci tre interi: ");		//dati richiesti
+  scanf("%d%d%d", &a, &b, &c);
+    
+  if (a>b) {					
+  	if (a>c) {									
+  		max = a;								
+  		if (c>b) {								
+  			min = b;							
+		  } else {								
+		  	min = c;							
+		  }										
+	  } else {									
+	  	max=c;									
+	  	min=b;										
+	  }											
+  } else {						
+  	if (b>c) {									
+  		max = b;								
+  		if (a>c) {								
+  			min=c;								
+		  } else {								
+		  	min=a;								
+			}									
+	  } else {									
+	  	max=c;									
+	  	min=a;									
+	  }											
+  }													
+  
+  	
+	printf("%d, %d", min, max);
+	return 0
+}
