5579d12440c6c1c0fc789aa6e7ca7f9db7ff1bee
l-savi <113691727+l-savi@users.noreply.github.com>
2022-10-28 18:18:32 +0200

    Add files via upload

---
+++ 001/a.minore.c
@@ -0,0 +1,33 @@
+/*/** ****************************************************************************************
+* \mainpage a.minore.c
+*
+* @brief Scrivere un programma che, richiesti in ingresso due valori interi distinti, ne stampi a schermo il minore.
+* 
+* 
+* @author Leonardo Savino
+* @date 26/10/2022
+*/
+
+
+#include <stdio.h>
+
+int main() 
+{
+  int num1, num2; //input
+  
+  printf ("inserisci due numeri interi distinti");
+  scanf ("%d %d", &num1, &num2);
+  
+  if (num1<num2)
+  {
+  	printf ("%d e' minore", num1);
+  }
+  
+   else 
+  {
+  	printf ("%d è minore", num2);
+  }
+  
+  
+  return 0;
+  }
