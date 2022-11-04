6b4d85859463a839320e0aedfa5978c993cdc4da
l-savi <113691727+l-savi@users.noreply.github.com>
2022-11-04 14:03:01 +0100

    Add files via upload

---
+++ es temperatura.cpp
@@ -0,0 +1,25 @@
+/*/** ****************************************************************************************
+* \mainpage switch
+*
+* @brief Leggi un numero che indica il valore di temperatura, in gradi Celsius 
+* e stampa un messaggio come da tabella a lato
+* 
+* @author Leonardo Savino
+* @date 04/11/2022
+* @version 1.0 04/11/22 Versione iniziale
+*/
+
+
+#include <stdio.h>
+
+int main() 
+{
+	float num;
+	
+	printf ("inserisci una temperatura");
+	scanf ("%f", &num);
+	
+	if ()
+	
+
+}
