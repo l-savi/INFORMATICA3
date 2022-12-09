2cb8dec2bdd0ff9a67af27d7f93de2a3854a4194
l-savi <113691727+l-savi@users.noreply.github.com>
2022-12-09 18:40:10 +0100

    Add files via upload

---
+++ ES02-Cicli e iterazioni/c_media.c
@@ -0,0 +1,34 @@
+/** ****************************************************************************************
+* \mainpage c_media.c
+*
+* @brief Si scriva un programma per calcolare la media aritmetica di una serie di numeri 
+*        inseriti da tastiera. L’introduzione del valore 0 indica il termine del caricamento 
+*        dei dati.
+* 
+* @author Leonardo Savino
+* @date 30/11/2022
+*/
+
+#include <stdio.h>      //inclusione librerie
+
+int main() 
+{
+    printf("\n--Esercizio C Leonardo Savino--\n\n");
+
+	int n, c=0, somma=0;      
+    float media=0;             
+    do 
+    {                                           //ciclo richiesta input e somma numeri inseriti
+        printf("Insersici un numero:");
+        scanf("%d", &n);
+
+        media += n;
+
+        if(n!=0) 
+            c++;                                
+    } while (n!=0);                         
+
+    media /= c;                 //calcolo media
+
+    printf("\nLa media dei numeri e' %.2f\n", media);
+}
