5f30fb4658b0c96dca389859beb5b4c2403c9ab3
l-savi <113691727+l-savi@users.noreply.github.com>
2022-12-09 19:23:13 +0100

    Add files via upload

---
+++ ES02-Cicli e iterazioni/f_Binario_decimale.c
@@ -0,0 +1,37 @@
+/** ****************************************************************************************
+* \mainpage 
+
+*
+* @brief Scrivere un programma che converte un numero binario a 8 bit in un numero decimale. 
+*        L’utente inserisce le cifre del numero binario un bit alla volta, partendo dal bit 
+*        più significativo. Il programma dovrà visualizzare il numero decimale corrispondente. 
+*        Suggerimento: per calcolare le potenze di 2 utilizzare la funzione pow includendo la 
+*        libreria math.h.
+* 
+* @authoLeonardo Savino
+* @date 09/12/2022
+*/
+
+
+#include <stdio.h>     
+#include <math.h>
+
+int main()  
+{
+    printf("\n--Esercizio F Leonardo Savino--\n\n");
+
+    
+    int bit, n=0, i=7;
+
+    printf("inserisci 8 bit\n");    //richiedo l'inserimento di 8 bit
+
+    do{
+        scanf("%d", &bit);          //leggo un bit alla volta
+
+        n += pow(2, i) * bit;    //converto singolo bit in decimale
+
+        i--;                        //decremento la potenza del 2 di 1
+    } while (i>-1);                 //continuo il ciclo fino a quando la potenza non raggiunge lo 0
+
+    printf("\nConversione in binario = %d\n", n);            // risultato
+}
