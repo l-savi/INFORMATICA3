2ca9fad485231514b309111aadb375791a2df272
l-savi <113691727+l-savi@users.noreply.github.com>
2022-12-09 18:33:58 +0100

    Update a_divisori.c

--- ES02-Cicli e iterazioni/a_divisori.c
+++ ES02-Cicli e iterazioni/a_divisori.c
@@ -1,1 +1,23 @@
-
+/* /** ******************************************************************************************
+* \mainpage a_divisori.c
+*
+* @brief Scrivere un programma che, richiesto un numero intero, visualizzi tutti i suoi divisori.
+* 
+*
+* @author Leonardo Savino
+* @date 07/12/2022
+*/
+#include  <stdio.h> 
+int main()
+{
+	int n, c;          // dichiaro le variabili
+	
+	printf ( "Inserisci un numero:" );	// richiesta input 
+	scanf ( "%d" , &n);
+	
+	for (c = 2 ; c < n; c++)		
+	{
+		if (n%c == 0 )								 // controllo se l'indice e' un divisore
+			printf ( "%d e' un divisore \n" , c);		
+	}	
+}
