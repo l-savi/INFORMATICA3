bf875ca01519c9634f50c38a8cc19ed2a25e362f
l-savi <113691727+l-savi@users.noreply.github.com>
2023-10-25 13:32:28 +0200

    Update FILE.cpp

--- FILEE.cpp
+++ FILEE.cpp
@@ -15,7 +15,7 @@ int main()
 
 void copiafile(char fin[],char fout[])
 {
-	FILE * err1;
+    FILE * err1;
     FILE * err2;
     char c;
     err1=fopen(fin,"r");
@@ -39,3 +39,40 @@ void copiafile(char fin[],char fout[])
 
 }    
 
+
+void copianome(char in[], char out[])
+{
+    FILE * err1;
+    FILE * err2;
+    char c;
+    err1=fopen(in,"r");
+    err2=fopen(out,"a");
+
+    if(err1 != 0 && err2 != 0)
+    {
+        printf("i file sono stati aperti correttamente\n");
+        while((c=fgetc(err1))!=EOF)
+        {
+            if(c >='a' && c<='z')
+            {
+                c-=32;
+            }
+            fputc(c,err2);
+        }
+
+        fclose(err1);
+        fclose(err2);
+    }
+    else
+    {
+        printf("file non aperti\n");
+    }
+}
+
+
+
+
+
+void conta(char in[], char fout[]){
+	
+}
