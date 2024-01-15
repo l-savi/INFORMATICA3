61e6d2f256c88cbde41edd6b56548d4fcd31f89a
l-savi <113691727+l-savi@users.noreply.github.com>
2024-01-15 09:34:19 +0100

    Add files via upload

---
+++ hello wordl java.cpp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+</html>
+
+<head>
+	<title>savino</title>
+</head>
+<body>
+
+<script>
+    window.alert( 'Hello, ' );
+    alert( ' world!' );
+    console.log("hello wordl")
+</script>
+
+</body>
+
+</html>
