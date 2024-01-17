e08f66dd26466c89b5103da9c1dfab938d518017
l-savi <113691727+l-savi@users.noreply.github.com>
2024-01-17 12:58:22 +0100

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+
+using namespace std;
+
+class Frazione {
+    private:
+        int numeratore;
+        int denominatore;
+
+    public:
+    	int aiuto;
+        Frazione () {}; 							//costuttore
+        void setnumeratore(int n)
+        {
+            numeratore=n;
+        }
+        void setdenominatore(int d)
+        {
+            denominatore=d;
+        }
+
+        int getnumeratore()
+        {
+            return numeratore;
+        }
+
+        int getdeniminatore()
+        {
+            return denominatore;
+        }
+        
+        void stampa()
+        {
+            cout<< numeratore << "/" << denominatore << endl;
+        }
+        void semplifica ()
+        {
+            int min;
+            min=MCD(numeratore,denominatore);
+            numeratore/=min;
+            denominatore/=min;
+        }
+
+        int MCD (int a,int b)
+        {
+	    int r=0;
+	    while(b!=0)
+	    {
+		    r=a%b;
+		    a=b;
+		    b=r;
+        } 
+	    return a;	
+        }   
+};
+
+int main ()
+{
+    Frazione f1;
+    int n,d;
+    f1.aiuto=4;
+	 
+    cout<<"inserisci prima il numeratore e poi il deniminatore"<<endl;
+    cin>>n;
+    cin>>d;
+    cout<<"\n"<<endl;
+    f1.setnumeratore(n);
+    f1.setdenominatore(d);
+    f1.stampa();
+
+    f1.semplifica();
+    f1.stampa();
+
+}
