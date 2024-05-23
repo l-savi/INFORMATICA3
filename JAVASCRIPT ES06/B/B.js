f5ae66db05b3320bb1bedc7844f5c2dd5fe5e003
l-savi <leoninoleonardo1990@gmail.com>
2024-05-23 15:24:33 +0000

    a

--- JAVASCRIPT ES06/B/B.js
+++ JAVASCRIPT ES06/B/B.js
@@ -1,5 +1,5 @@
-const text = document.getElementById("Input");       //alla costante andiamo ad avvolare l'id dell'elemento su quale andiamo a lavorare e tramite quel comando andiamo a prendere l'id su quale andiamo a lavorare
+const text = document.getElementById("Input");       
 
-text.addEventListener("input", function(){           //evento al testo
-    text.value = text.value.toUpperCase()            //il testo diventa maiuscolo, con quella funzione
+text.addEventListener("input", function(){  
+    text.value = text.value.toUpperCase()     
 });
