6dee527ec43c523b46290b6dbf5ce3dfcac0e6d0
l-savi <leoninoleonardo1990@gmail.com>
2024-05-23 15:50:39 +0000

    a

--- JAVASCRIPT ES06/E/E.js
+++ JAVASCRIPT ES06/E/E.js
@@ -1,7 +1,7 @@
 const password1 = document.getElementById("password1");
 const password2 = document.getElementById("password2");
 
-pass2.addEventListener("change", function() {        //EVENTO
+password2.addEventListener("change", function() {        //EVENTO
     if(password1.value!== password2.value){         //NON UGUALI
         alert("Le passwords non sono uguali");      //avviso
     }    
