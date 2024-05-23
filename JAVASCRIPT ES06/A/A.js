74b6873fb4536faa6e4c3147cbfabf7a9399ef18
l-savi <leoninoleonardo1990@gmail.com>
2024-05-23 13:50:38 +0000

    a

--- JAVASCRIPT ES06/A/A.js
+++ JAVASCRIPT ES06/A/A.js
@@ -1,9 +1,10 @@
-const button = document.getElementById("myButton");
+//Creo script che poi viene eseguito con Google
+const Button = document.getElementById("Button");    //alla costante andiamo ad avvolare l'id dell'elemento su quale andiamo a lavorare e tramite quel comando andiamo a prendere l'id su quale andiamo a lavorare
 
-button.addEventListener('mouseover', function(){
-    button.style.backgroundColor = "lightgreen";
+Button.addEventListener('mouseover', function(){                   //evento al bottone con mouse sul bottone
+    Button.style.backgroundColor = "lightgreen";                   //prendiamo stile bottone e cambiamo sfondo 
 });
 
-button.addEventListener('mouseout', function(){
-    button.style.backgroundColor = "white";
+Button.addEventListener('mouseout', function(){                     //evento al bottone con mouse non sul bottone 
+    Button.style.backgroundColor = "white";                            //prendiamo stile bottone colore sfondo 
 });
