781541478a864a7393b15923cc7e395c2ca4eeb3
l-savi <leoninoleonardo1990@gmail.com>
2024-05-24 13:58:28 +0000

    a

--- JAVASCRIPT ES05/C/C.js
+++ JAVASCRIPT ES05/C/C.js
@@ -1,10 +1,10 @@
-const bottone = document.getElementById("bottone1");
-const bottone2 = document.getElementById("bottone2");
+const Bottone = document.getElementById("Bottone1");
+const Bottone2 = document.getElementById("Bottone2");
 
 bottone1.style.backgroundColor = "lightgreen";
 bottone1.style.border ="green";
 bottone1.style.color = "red";
 
 bottone2.style.backgroundColor = "lightblue";
-bottone2.style.border ="green";
-bottone2.style.color = "violet";
+bottone2.style.border ="yellow";
+bottone2.style.color = "pink";
