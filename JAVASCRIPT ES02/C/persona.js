d50a7840983ff80e604b09a3323b3dfb05724f5b
l-savi <113691727+l-savi@users.noreply.github.com>
2024-05-24 15:29:40 +0200

    Create persona.js

---
+++ JAVASCRIPT ES02/C/persona.js
@@ -0,0 +1,9 @@
+persona = {}
+
+persona.nome = prompt("Inserisci il nome")
+persona.cognome = prompt("Inserisci il cognome")
+persona.eta = prompt("Inserisci l'eta")
+
+console.log("Nome = " + persona.nome);
+console.log("Cognome = " + persona.cognome);
+console.log("Eta'= " + persona.età);
