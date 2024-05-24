ad8e1c9247175fbd1f87b7b946c06c217ab0076a
l-savi <113691727+l-savi@users.noreply.github.com>
2024-05-24 15:11:09 +0200

    Create C.js

---
+++ JAVASCRIPT ES05/C/C.js
@@ -0,0 +1,10 @@
+const bottone = document.getElementById("bottone1");
+const bottone2 = document.getElementById("bottone2");
+
+bottone1.style.backgroundColor = "lightgreen";
+bottone1.style.border ="green";
+bottone1.style.color = "red";
+
+bottone2.style.backgroundColor = "lightblue";
+bottone2.style.border ="green";
+bottone2.style.color = "violet";
